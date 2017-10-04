9890469d1a0f873d1d75633bd9846ca5b685dd8a
Daniel Wang <zwang719@mtroyal.ca>
2017-10-04 00:09:37 -0600

    try to load user session to dashboard page

--- app/src/main/java/com/prog4/wangz_jamileh/wishlist/DashboardActivity.java
+++ app/src/main/java/com/prog4/wangz_jamileh/wishlist/DashboardActivity.java
@@ -2,14 +2,21 @@ package com.prog4.wangz_jamileh.wishlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.prog4.wangz_jamileh.wishlist.R;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    private String session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Bundle extra = getIntent().getExtras();
+        TextView textView = (TextView) findViewById(R.id.session);
+        if(extra != null){
+            session = (String) extra.get("session");
+            textView.setText(session);
+        }
     }
 }
