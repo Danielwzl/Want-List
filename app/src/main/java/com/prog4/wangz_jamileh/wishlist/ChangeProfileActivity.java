63d449f13f8787517779ef72379e7df752cc89a2
Daniel Wang <daniel@acuspire.com>
2017-10-22 01:09:38 -0600

    WIP two-way binding

--- app/src/main/java/com/prog4/wangz_jamileh/wishlist/ChangeProfileActivity.java
+++ app/src/main/java/com/prog4/wangz_jamileh/wishlist/ChangeProfileActivity.java
@@ -1,16 +1,59 @@
 package com.prog4.wangz_jamileh.wishlist;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.prog4.wangz_jamileh.wishlist.Model.User;
 import com.prog4.wangz_jamileh.wishlist.R;
+import com.prog4.wangz_jamileh.wishlist.databinding.ActivityChangeProfileBinding;
 
 public class ChangeProfileActivity extends AppCompatActivity {
+    private User user;
+    private String dob;
+    private EditText dobView;
+    public static final int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_profile);
+        user = User.getInstance();
+        ActivityChangeProfileBinding bind = DataBindingUtil.setContentView(this, R.layout.activity_change_profile);
+        bind.setUser(user);
 
+        dobView = (EditText) findViewById(R.id.changeProfile_dob);
+        dobView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
+            }
+        });
     }
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID) {
+            int[] dob = user.getAllfieldsFromDOB();
+            return new DatePickerDialog(this, dpickerListner, dob[0], dob[1], dob[2]);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            month += 1;
+            dob = year + "/" + month + "/" + dayOfMonth;
+            Toast.makeText(ChangeProfileActivity.this, dob, Toast.LENGTH_SHORT).show();
+            dobView.setText(dob);
+        }
+    };
 }
--- app/src/main/java/com/prog4/wangz_jamileh/wishlist/Model/User.java
+++ app/src/main/java/com/prog4/wangz_jamileh/wishlist/Model/User.java
@@ -1,14 +1,106 @@
 package com.prog4.wangz_jamileh.wishlist.Model;
 
-/**
- * Created by ZILONG on 2017-10-21.
- */
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import com.prog4.wangz_jamileh.wishlist.magic.Ajax;
 
-public class User {
+public class User extends BaseObservable{
+    public static User user = new User();
 
-    public String username, fname, lname, dob, phone, email, session, gender;
+    public String username = "daniel", fname, lname, dob = "1988/12/02", phone, email, session, gender, fullName = "Daniel Wang";
 
     public User(){
+
+    }
+
+    public static User getInstance(){
+        if(user != null) return user;
+        else return user = new User();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @Bindable
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+//        notifyPropertyChanged(BR.dob);
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int[] getAllfieldsFromDOB(){
+        int[] fields = {2017, 1, 1};
+        if(dob != null) {
+            String[] dobAry = dob.split("/");
+            fields[0] = Integer.parseInt(dobAry[0]) - 1;
+            fields[1] = Integer.parseInt(dobAry[1]);
+            fields[2] = Integer.parseInt(dobAry[2]);
+        }
+        return fields;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    private void getData(){
+        Ajax a = new Ajax();
 
     }
 }
