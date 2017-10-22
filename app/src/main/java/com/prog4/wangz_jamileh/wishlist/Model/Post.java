c9e337b432301464e66b95a69409f41ac4f5758b
Daniel Wang <daniel@acuspire.com>
2017-10-21 19:36:11 -0600

    create two object to keep track of change for the data binding usage

---
+++ app/src/main/java/com/prog4/wangz_jamileh/wishlist/Model/Post.java
@@ -0,0 +1,8 @@
+package com.prog4.wangz_jamileh.wishlist.Model;
+
+/**
+ * Created by ZILONG on 2017-10-21.
+ */
+
+public class Post {
+}
---
+++ app/src/main/java/com/prog4/wangz_jamileh/wishlist/Model/User.java
@@ -0,0 +1,14 @@
+package com.prog4.wangz_jamileh.wishlist.Model;
+
+/**
+ * Created by ZILONG on 2017-10-21.
+ */
+
+public class User {
+
+    public String username, fname, lname, dob, phone, email, session, gender;
+
+    public User(){
+
+    }
+}
