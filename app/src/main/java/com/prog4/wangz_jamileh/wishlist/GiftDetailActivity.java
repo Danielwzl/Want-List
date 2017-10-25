4f4bc9ac0251dda4ce5abb3d96f0c10275e435db
Daniel Wang <daniel@acuspire.com>
2017-10-25 11:52:13 -0600

    create 3 blank activity
    detail, view gift and search user

---
+++ app/src/main/java/com/prog4/wangz_jamileh/wishlist/GiftDetailActivity.java
@@ -0,0 +1,15 @@
+package com.prog4.wangz_jamileh.wishlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prog4.wangz_jamileh.prog4.R;
+
+public class GiftDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift_detail);
+    }
+}
